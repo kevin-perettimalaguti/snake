412ed603f9a55b340d01da39af38049b6d498334
kevin.perettimalaguti <kevin.peretti-malaguti@laplateforme.io>
2023-12-21 13:34:29 +0100

    rajout de deux fonction logique a l'ia

--- ia.py
+++ ia.py
@@ -23,8 +23,46 @@ class IA:
         elif diff_y < 0 and (tete_x, tete_y - 1) not in self.jeu.serpent.corps[1:]:
             return (0, -1)  # Aller vers le haut
         else:
-            # Si la tête est alignée avec la nourriture, choisis une direction aléatoire
-            return random.choice(self.directions)
+            # Si la tête est alignée avec la nourriture, évite les zones dangereuses
+            return self.eviter_zones_dangereuses()
+
+    def esquiver_bords(self):
+        tete_x, tete_y = self.jeu.serpent.corps[0]
+        directions_disponibles = []
+
+        # Vérifie les directions disponibles pour éviter les bords
+        if tete_x > 0 and (tete_x - 1, tete_y) not in self.jeu.serpent.corps[1:]:
+            directions_disponibles.append((-1, 0))  # Gauche
+        if tete_x < self.jeu.nombre_cases - 1 and (tete_x + 1, tete_y) not in self.jeu.serpent.corps[1:]:
+            directions_disponibles.append((1, 0))  # Droite
+        if tete_y > 0 and (tete_x, tete_y - 1) not in self.jeu.serpent.corps[1:]:
+            directions_disponibles.append((0, -1))  # Haut
+        if tete_y < self.jeu.nombre_cases - 1 and (tete_x, tete_y + 1) not in self.jeu.serpent.corps[1:]:
+            directions_disponibles.append((0, 1))  # Bas
+
+        # Choisi une direction aléatoire parmi celles disponibles
+        return random.choice(directions_disponibles)
+
+    def eviter_zones_dangereuses(self):
+        tete_x, tete_y = self.jeu.serpent.corps[0]
+        directions_disponibles = []
+
+        # Vérifie les directions disponibles pour éviter les zones dangereuses
+        for direction in self.directions:
+            prochaine_position = (tete_x + direction[0], tete_y + direction[1])
+
+            # Évite les positions à proximité des bords
+            if (
+                prochaine_position[0] > 1
+                and prochaine_position[0] < self.jeu.nombre_cases - 2
+                and prochaine_position[1] > 1
+                and prochaine_position[1] < self.jeu.nombre_cases - 2
+                and prochaine_position not in self.jeu.serpent.corps[1:]
+            ):
+                directions_disponibles.append(direction)
+
+        # Choisi une direction aléatoire parmi celles disponibles
+        return random.choice(directions_disponibles)
 
     def jouer(self):
         direction = self.choisir_direction()
